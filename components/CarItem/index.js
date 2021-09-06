b96318d164da346df2d443915c65c6a4414d3db2
vaibhav dharmik <vaibhav.crewbyte@gmail.com>
2021-09-06 15:11:30 +0530

    Basic Setup

---
+++ components/CarItem/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const CarItem = () => {
+  return (
+    <View>
+      <Text>Lets Go Vaibhav🚀 🚀 🚀 🚀</Text>
+    </View>
+  );
+};
+
+export default CarItem;
